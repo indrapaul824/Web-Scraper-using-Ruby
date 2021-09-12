aef5cd5a5ceb511930c4e98f9e66a53f178767c1
IndraP24 <indrashis985@gmail.com>
2021-09-12 23:58:09 +0530

    Add links extraction from parsed text

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,19 @@
 require 'watir'
 require 'webdrivers'
 require 'nokogiri'
+
+
+browser = Watir::Browser.new
+browser.goto 'https://blog.eatthismuch.com/latest-articles/'
+parsed_page = Nokogiri::HTML(browser.html)
+
+File.open("parsed.txt", "w") { |f| f.write "#{parsed_page}" }
+
+browser.close
+
+puts parsed_page.title
+
+links = parsed_page.css('a')
+links.map { |element| element["href"] }
+
+puts links
